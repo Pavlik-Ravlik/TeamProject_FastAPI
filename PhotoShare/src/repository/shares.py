97bd1b55debd821f14709323bc0795cf7f394a38
avegigs <125361614+avegigs@users.noreply.github.com>
2023-10-15 12:07:32 +0300

    add generate_qr_code

--- PhotoShare/src/repository/shares.py
+++ PhotoShare/src/repository/shares.py
@@ -1,14 +1,14 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import text, and_
 
-from fastapi import HTTPException
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import Type
 
 from src.database.models import User, Share
 from schemas import ShareRequest
-
-
+import qrcode as qr
+import cloudinary.uploader
 
 
 
@@ -63,3 +63,35 @@ async def delete_share(share_id: int, db: Session, current_user: User) -> Type[S
     db.delete(share)
     db.commit()
     return share 
+
+# qrcode
+async def generate_qr_code(url: str, name: str, db: Session, current_user: User) -> Share:
+    share = db.query(Share).filter(and_(Share.url == url, Share.user_id == current_user.id)).first()
+
+    if share is None:
+        raise HTTPException(status_code=404, detail='Share not found')
+
+    combined_data = f"{name}\n{url}"
+
+    image_qr = qr.QRCode(
+        error_correction=qr.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4
+    )
+
+    image_qr.add_data(combined_data)
+    image_qr.make(fit=True)
+
+    qr_code_data = image_qr.make_image(fill_color="black", back_color="white")
+    qr_code_data = qr_code_data.get_image()  # 
+
+    cloudinary_response = cloudinary.uploader.upload(qr_code_data.to_file(), folder="qr_codes")
+    image_url = cloudinary_response["secure_url"]
+
+    share.image_qr = image_url
+
+    db.commit()
+    db.refresh(share)
+
+    return qr_code_data# qr_code_data.print_ascii(tty=True, invert=True)
+
